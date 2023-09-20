85f02b2db1ccfe5543249277a0d8f6263e2458f9
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-09-20 14:44:19 -0300

    resolve req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("message", "olá")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(123, 1)
+    assert encrypt_message("message", 1) == "e_messag"
+
+    assert encrypt_message("message", 18) == "egassem"
